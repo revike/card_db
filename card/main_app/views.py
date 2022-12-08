4f5468c9b397f852fc06909e2ce3a6e1bd84b8f1
revike <revike@ya.ru>
2022-12-08 16:24:43 +0300

    auth admin

--- card/main_app/views.py
+++ card/main_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    """Index view"""
+    template_name = 'main_app/index.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
