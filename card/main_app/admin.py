67d951ca2b2c7af30947be327482cbc799ec7047
revike <revike@ya.ru>
2022-12-08 23:23:52 +0300

    models and start views

--- card/main_app/admin.py
+++ card/main_app/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from main_app.models import HistoryCard, Card, ProfileCard
+
+
+class ProfileCardInline(admin.StackedInline):
+    """Profile card inline"""
+    model = ProfileCard
+    can_delete = False
+
+
+class HistoryCardInline(admin.StackedInline):
+    """History Card Inline"""
+    model = HistoryCard
+    extra = 0
+    max_num = 0
+    min_num = 0
+    can_delete = False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    """Card Admin"""
+    inlines = [ProfileCardInline, HistoryCardInline]
+    inline_reverse = ['card']
+    list_display = ('series', 'numbers', 'is_active', 'overdue', 'is_delete')
+    list_display_links = ('series', 'numbers')
+    fields = (
+        'series', 'numbers', 'release_data', 'term', 'amount',
+        'is_active',
+        'overdue', 'is_delete')
+    readonly_fields = (
+        'series', 'numbers', 'release_data', 'term', 'amount',
+        'overdue')
+    list_filter = ('series', 'term', 'is_active', 'overdue', 'is_delete',)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+
+admin.site.unregister(Group)
+admin.site.site_header = 'Бонусные карты'
--- card/main_app/models.py
+++ card/main_app/models.py
@@ -1,3 +1,76 @@
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Card(models.Model):
+    """Model card"""
+    TERM_CHOICES = (
+        ('1', '1 месяц'),
+        ('6', '6 месяцев'),
+        ('12', '1 год'),
+    )
+    series_valid = RegexValidator(regex='^\d{4}$')
+    series = models.PositiveSmallIntegerField(
+        validators=[series_valid], verbose_name='серия',
+        error_messages='серия карты состоит из 4х цифр')
+    numbers = models.PositiveBigIntegerField(verbose_name='номер')
+    release_data = models.DateTimeField(auto_now_add=True,
+                                        verbose_name='дата выпуска')
+    term = models.PositiveSmallIntegerField(choices=TERM_CHOICES,
+                            verbose_name='срок')
+    end_activity = models.DateTimeField(
+        verbose_name='дата окончания активности')
+    amount = models.DecimalField(max_digits=10,
+                                 decimal_places=2, default=0,
+                                 verbose_name='сумма',
+                                 validators=[
+                                     MinValueValidator(Decimal('0.00'))])
+    is_active = models.BooleanField(default=False, verbose_name='активирована')
+    overdue = models.BooleanField(default=False, verbose_name='просрочена')
+    is_delete = models.BooleanField(default=False, verbose_name='удалена')
+
+    class Meta:
+        db_table = "card"
+        verbose_name = "Карта"
+        verbose_name_plural = "Карты"
+
+    def __str__(self):
+        return f'{self.series} №{self.numbers}'
+
+
+class ProfileCard(models.Model):
+    """Profile Card"""
+    objects = None
+    card = models.OneToOneField(Card, on_delete=models.CASCADE,
+                                verbose_name='карта')
+    first_name = models.CharField(max_length=128, verbose_name='имя')
+    last_name = models.CharField(max_length=128, verbose_name='фамилия')
+    email = models.EmailField(verbose_name='email')
+    phone_number_regex = RegexValidator(regex=r"\+7\d{10,10}$")
+    phone = models.CharField(validators=[phone_number_regex], max_length=12,
+                             blank=True, verbose_name='телефон')
+
+    class Meta:
+        db_table = "profile"
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class HistoryCard(models.Model):
+    """Date of use card"""
+    card = models.ForeignKey(Card, on_delete=models.CASCADE,
+                             verbose_name='карта')
+    data_use = models.DateTimeField(verbose_name='дата использования')
+
+    class Meta:
+        db_table = "history_card"
+        verbose_name = "История карты"
+        verbose_name_plural = "История карты"
+
+    def __str__(self):
+        return f'{self.card}'
